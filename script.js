c482f04c1fcfa10a2af8fd31a03b6f3ec7ff585d
ShadySoltan <shady.aly.soltan@gmail.com>
2024-12-06 13:17:08 +0200

    update buttons and adding links

--- script.js
+++ script.js
@@ -5,3 +5,10 @@ menuIcon.onclick = () => {
     menuIcon.classList.toggle('bx-x')
     navbar.classList.toggle('active');
 }
+
+document.getElementById('contact-btn').onclick = function (event) {
+    event.preventDefault(); // Prevent default button behavior
+    document.querySelector('#contact').scrollIntoView({
+        behavior: 'smooth' // Smooth scroll effect
+    });
+};
